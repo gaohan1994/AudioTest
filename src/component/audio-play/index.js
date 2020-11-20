4e738c81f67fa5d1e24c538e9b0b3bff821a22f8
gaohan1994 <871418277@qq.com>
2020-11-20 15:49:22 +0800

    * 新增 加入sound组件 新增播放录音功能

--- src/component/audio-play/index.js
+++ src/component/audio-play/index.js
@@ -2,16 +2,35 @@ import React from 'react';
 import Modal from 'react-native-modal';
 import {View, Text} from 'react-native';
 import {Button} from 'react-native-centerm-sdk';
+import Sound from 'react-native-sound';
+import {Toast} from 'teaset';
 
 export default (props) => {
   const {visible, setVisible, url} = props;
 
   const onPlayAudio = () => {
     console.log('url', url);
+    const sound = new Sound(url, '', (error) => {
+      if (error) {
+        console.log('sound error:', error);
+      }
+
+      setTimeout(() => {
+        sound.play((success) => {
+          if (success) {
+            console.log('successfully finished playing');
+            Toast.success('播放完毕！');
+            setVisible(false);
+          } else {
+            console.log('playback failed due to audio decoding errors');
+          }
+        });
+      }, 100);
+    });
   };
 
   return (
-    <Modal isVisible={true} onBackButtonPress={() => setVisible(false)}>
+    <Modal isVisible={visible} onBackButtonPress={() => setVisible(false)}>
       <View style={{backgroundColor: '#fff', padding: 20, borderRadius: 10}}>
         <Text>是否播放刚才通信的录音？</Text>
 
